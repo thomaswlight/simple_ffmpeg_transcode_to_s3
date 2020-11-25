29cc0c1ee9a5255b738ec8f580024d5711b2dd01
thomaswlight <thomas.w.light@gmail.com>
2020-11-24 23:09:24 -0500

    Add files via upload

--- copy_output.sh
+++ copy_output.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+printenv
 echo "Copying ${OUTPUT_FILE_NAME} to S3 at ${OUTPUT_S3_PATH}/${OUTPUT_FILE_NAME} ..."
 aws s3 cp ./${OUTPUT_FILE_NAME} s3://${OUTPUT_S3_PATH}/${OUTPUT_FILE_NAME} --region ${AWS_REGION}
